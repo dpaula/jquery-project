85b2f7f46aff6587bff612dea04a99a95751c0b9
dpaula <fernando.dpaula@gmail.com>
2018-03-25 22:16:42 -0300

    usando .text() do jquery para pegar valores dos campos do document

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,11 @@
-console.log('funcionando..');
+
+//$ equivale ao document.querySelector(.classe)
+//metodo .text() pega o texto, já para inserir basta passar o conteudo text(conteudo)
+let frase = $('.frase').text();
+
+const qtdPalavras =frase.split(' ').length;
+
+let numeroPalavras = $('#numero-palavras');
+
+numeroPalavras.text(qtdPalavras);
+
