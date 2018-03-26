8de9f0c3e2d3c5ca7523e5e6023dfe1a541eb8d6
dpaula <fernando.dpaula@gmail.com>
2018-03-26 00:09:53 -0300

    declarando eventos e setando valores com jquery

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,15 @@ let numeroPalavras = $('#numero-palavras');
 
 numeroPalavras.text(qtdPalavras);
 
+let campoD = $('.campo-digitacao');
+//para setar o evento com jquery, basta usar o metodo on
+//passando o primeiro parametro qual sera o evento, e o segundo a função que sera executada
+campoD.on('click', () => {
+    //para pegar valor de um textArea, ao inves de .value(), no jquery é usado o .val()
+    //a diferença em CAMPO.text() para CAMPO.val() é que text é o texto entre tags 
+    //e o value() é o que está dentro de caixas de input
+    let textoCampoArea = campoD.val();
+    let qtdPalavrasTA = textoCampoArea.split(' ').length;
+    $('#contador-palavras').text(qtdPalavrasTA);
+});
+
