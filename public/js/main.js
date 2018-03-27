2977a713ad78285c507badf683374891f4778503
dpaula <fernando.dpaula@gmail.com>
2018-03-26 22:33:59 -0300

    reinicializacao do jogo, usando ready para iniar as funncoes ao atualizar a tela e organizado em funcoes

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,63 @@
-//$ equivale ao document.querySelector(.classe)
-//metodo .text() pega o texto, já para inserir basta passar o conteudo text(conteudo)
-let frase = $('.frase').text();
-
-const qtdPalavras = frase.split(' ').length;
-
-let numeroPalavras = $('#numero-palavras');
-
-numeroPalavras.text(qtdPalavras);
-
+const tempo = 3;
+const qtdPalavras = $('.frase').text().split(' ').length;
+$('#tempo-digitacao').text(tempo);
 let campoD = $('.campo-digitacao');
-//para setar o evento com jquery, basta usar o metodo on
-//passando o primeiro parametro qual sera o evento, e o segundo a função que sera executada
-campoD.on('input', () => {
-    //para pegar valor de um textArea, ao inves de .value(), no jquery é usado o .val()
-    //a diferença em CAMPO.text() para CAMPO.val() é que text é o texto entre tags 
-    //e o value() é o que está dentro de caixas de input
-    let textoCampoArea = campoD.val();
-    let qtdPalavrasTA = textoCampoArea.split(/\S+/).length - 1;
-    $('#contador-palavras').text(qtdPalavrasTA);
 
-
-    let qtdCaracteres = textoCampoArea.length;
-    $('#contador-caracteres').text(qtdCaracteres);
+//onReady - carrega a tela e só dai executa a função
+$('document').ready(() => {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $('#botao-reiniciar').click(reiniciaJogo);
 });
 
-let tempoDigitacao = $('#tempo-digitacao').text();    
 
-//criando evento apenas uma vez
-//campo.one('evento', função)
-campoD.one('focus', () => {
 
-    //usando setInterval para definir uma função em um espaço de tempo desejado
-    //e atribuindo ao um ID, que pode ser parado quando quiser
-    //funciona com dois parametros(função, tempo em milisegundos)
-    let cronometroId = setInterval(() => {
-        tempoDigitacao--;
+function reiniciaJogo() {
+    campoD.attr('disabled', false);
+    campoD.val('');
+    $('#contador-palavras').text('0');
+    $('#contador-caracteres').text('0');
+    $('#tempo-digitacao').text(tempo);
+    $('#numero-palavras').text(qtdPalavras);
+    inicializaCronometro();
+}
 
-        $('#tempo-digitacao').text(tempoDigitacao);
+function atualizaTamanhoFrase() {
+    let numeroPalavras = $('#numero-palavras');
+    numeroPalavras.text(qtdPalavras);
+}
 
-        if(tempoDigitacao < 1){
+function inicializaContadores() {
+    campoD.on('input', () => {
+        //para pegar valor de um textArea, ao inves de .value(), no jquery é usado o .val()
+        //a diferença em CAMPO.text() para CAMPO.val() é que text é o texto entre tags 
+        //e o value() é o que está dentro de caixas de input
+        let textoCampoArea = campoD.val();
+        let qtdPalavrasTA = textoCampoArea.split(/\S+/).length - 1;
+        $('#contador-palavras').text(qtdPalavrasTA);
+        let qtdCaracteres = textoCampoArea.length;
+        $('#contador-caracteres').text(qtdCaracteres);
+    });
+}
 
-            //metodo campo.attr que servve para pegar os atributos dos campos e setar valores
-            campoD.attr('disabled', true);
-            //para o interval
-            clearInterval(cronometroId);
-        }
-
-    }, 1000);
-
-});
+function inicializaCronometro() {
+    let tempoRestante = $('#tempo-digitacao').text();
+    //criando evento apenas uma vez
+    //campo.one('evento', função)
+    campoD.one('focus', () => {
+        //usando setInterval para definir uma função em um espaço de tempo desejado
+        //e atribuindo ao um ID, que pode ser parado quando quiser
+        //funciona com dois parametros(função, tempo em milisegundos)
+        let cronometroId = setInterval(() => {
+            tempoRestante--;
+            $('#tempo-digitacao').text(tempoRestante);
+            if (tempoRestante < 1) {
+                //metodo campo.attr que servve para pegar os atributos dos campos e setar valores
+                campoD.attr('disabled', true);
+                //para o interval
+                clearInterval(cronometroId);
+            }
+        }, 1000);
+    });
+}
