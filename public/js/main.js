4373e0fd8a992b04705de19dcc3d98dfd33e23dc
dpaula <fernando.dpaula@gmail.com>
2018-03-26 21:42:52 -0300

    usando evento unico, setInterval atribuindo a um ID e parando o tempo, e o metodo campo.attr() para pegar os atributos dos campos

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,8 @@
-
 //$ equivale ao document.querySelector(.classe)
 //metodo .text() pega o texto, já para inserir basta passar o conteudo text(conteudo)
 let frase = $('.frase').text();
 
-const qtdPalavras =frase.split(' ').length;
+const qtdPalavras = frase.split(' ').length;
 
 let numeroPalavras = $('#numero-palavras');
 
@@ -17,7 +16,7 @@ campoD.on('input', () => {
     //a diferença em CAMPO.text() para CAMPO.val() é que text é o texto entre tags 
     //e o value() é o que está dentro de caixas de input
     let textoCampoArea = campoD.val();
-    let qtdPalavrasTA = textoCampoArea.split(/\S+/).length -1;
+    let qtdPalavrasTA = textoCampoArea.split(/\S+/).length - 1;
     $('#contador-palavras').text(qtdPalavrasTA);
 
 
@@ -25,3 +24,28 @@ campoD.on('input', () => {
     $('#contador-caracteres').text(qtdCaracteres);
 });
 
+let tempoDigitacao = $('#tempo-digitacao').text();    
+
+//criando evento apenas uma vez
+//campo.one('evento', função)
+campoD.one('focus', () => {
+
+    //usando setInterval para definir uma função em um espaço de tempo desejado
+    //e atribuindo ao um ID, que pode ser parado quando quiser
+    //funciona com dois parametros(função, tempo em milisegundos)
+    let cronometroId = setInterval(() => {
+        tempoDigitacao--;
+
+        $('#tempo-digitacao').text(tempoDigitacao);
+
+        if(tempoDigitacao < 1){
+
+            //metodo campo.attr que servve para pegar os atributos dos campos e setar valores
+            campoD.attr('disabled', true);
+            //para o interval
+            clearInterval(cronometroId);
+        }
+
+    }, 1000);
+
+});
